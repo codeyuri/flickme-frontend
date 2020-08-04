cdd963b1369190f74c5c23e70d10e4367975d406
eugene <eugenepdm@gmail.com>
2020-08-05 00:25:47 +0800

    new updates

---
+++ src/components/OnlineUsersListContainer.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+import "./OnlineUsers.styles.css";
+
+const OnlineUsersLinkContainer = () => {
+  return (
+    <div>
+      <div className="circle-icon"></div>
+      <li>TEST</li>
+    </div>
+  );
+};
+
+export default OnlineUsersLinkContainer;
